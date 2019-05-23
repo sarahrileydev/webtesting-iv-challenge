77fdcf70ddddc23c72f34b068dbce0e105f36932
sriley <sarahriley.dev@gmail.com>
2019-05-23 17:12:15 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
-const express = require('express');
+const express = require("express");
+
+const books = require("../books/booksModel");
+
 const server = express();
 
 server.use(express.json());
 
+server.get("/", async (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+server.get("/hobbits", async (req, res) => {
+  const rows = await books.getAll();
+
+  res.status(200).json(rows);
+});
 
 module.exports = server;
